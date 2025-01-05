c2a0271e7dc928bdcb23ebcbfc7569d7745c8763
Nyrew <lukas.vokel@gmail.com>
2025-01-05 18:09:09 +0100

    change of main.py with path to crud

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from scraper.config import *
 from scraper.scraper import scrape
 from database.database import get_db, engine
 from database.models import Base
-from database.crud import save_scraped_data, get_all_data
+from backend.database.crud import save_scraped_data, get_all_data
 
 app = FastAPI()
 
